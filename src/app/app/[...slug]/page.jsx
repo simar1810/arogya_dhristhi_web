043136a4b9513ca339a1c5ad032d286108b1e339
Ayush Thukral <krishayush45@gmail.com>
2025-08-11 19:50:01 +0530

    changes

--- src/app/app/[...slug]/page.jsx
+++ src/app/app/[...slug]/page.jsx
@@ -5,15 +5,12 @@ export default function Page() {
   return <section className="w-full min-h-screen bg-[url('/app_bg.png')] bg-center flex justify-center items-center py-[4rem] px-6 md:px-12 ">
     <div className="flex flex-col lg:flex-row justify-between items-center h-full text-white gap-[2rem] lg:gap-[5rem]">
       <div className="lg:min-w-1/2 flex flex-col items-center lg:items-start lg:pl-6 md:mt-[10rem] lg:mt-0 text-center lg:text-left">
-        <h1 className="text-[1.2rem] md:text-[1.5rem] lg:text-[3rem] font-bold lg:mb-12 lg:leading-[3.5rem] ">Download the <br />
-          <span className='md:text-[2.7rem] text-[2rem] lg:text-[3rem] text-white opacity-100'>The Zen Fit App</span>
-        </h1>
         <p className="text-sm md:text-xl mb-5 opacity-70 ">Begin your journey Towards Making People Healthy</p>
         <div className="flex justify-between items-center gap-5 mb-5">
-          <Link href="https://play.google.com/store/apps/details?id=com.wellnessz.zenfit&hl=en-us" target="_blank">
+          <Link href="https://play.google.com/store/apps/details?id=com.wellnessz.deepika " target="_blank">
             <Image src="/playstore.png" alt="Google Store" width={150} height={40} className="cursor-pointer" />
           </Link>
-          <Link href="https://apps.apple.com/us/app/zenfit-app/id6743387199" target="_blank">
+          <Link href="https://apps.apple.com/in/app/wellnessz/id6478812964" target="_blank">
             <Image src="/appStore.png" alt="App Store" width={150} height={60} className="cursor-pointer" />
           </Link>
         </div>
